7f8aa22be1cd9b0cf64e981ae5e460c131a64b50
Eugene <zhenya.dunin1998@mail.ru>
2019-08-29 19:50:57 +0300

    Add project with unit tests.

---
+++ EugeneDunin.DataModule/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b11d39ea-ce39-4a90-8d73-b61b718fab2e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EugeneDunin.DataModule/UnitTestProject/TeacherWorkloadProviderUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EugeneDunin.SchoolSchedule.DataModule.Contexts;
+using EugeneDunin.SchoolSchedule.Foundation.Interfaces;
+using EugeneDunin.SchoolSchedule.Foundation.TeacherWorkloads;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TeacherWorkloadProviderUnitTest
+    {
+        private readonly SchoolScheduleContext _schoolScheduleContext;
+        private readonly ITeacherWorkloadProvider _teacherWorkloadProvider;
+        private readonly ITeacherWorkloadFactory _teacherWorkloadFactory;
+
+        public TeacherWorkloadProviderUnitTest()
+        {
+            _schoolScheduleContext = new SchoolScheduleContext();
+            _teacherWorkloadFactory = new TeacherWorkloadFactory();
+            _teacherWorkloadProvider = new TeacherWorkloadProvider(_schoolScheduleContext, _teacherWorkloadFactory);
+        }
+
+        [TestMethod]
+        public void QueryTest()
+        {
+
+        }
+    }
+}
