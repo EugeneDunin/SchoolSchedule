c3f57a50df881fa80b3e634dc6e5a84b74a8a2dc
Eugene <zhenya.dunin1998@mail.ru>
2019-09-02 13:56:36 +0300

    Fix implementation of TeacherWorkloadProvider & ClassFactory classes.

--- EugeneDunin.DataModule/EugeneDunin.SchoolSchedule.Foundation/Classes/ClassFactory.cs
+++ EugeneDunin.DataModule/EugeneDunin.SchoolSchedule.Foundation/Classes/ClassFactory.cs
@@ -1,12 +1,13 @@
-﻿using EugeneDunin.SchoolSchedule.Foundation.Interfaces;
+﻿using EugeneDunin.SchoolSchedule.DataModule.Contexts;
+using EugeneDunin.SchoolSchedule.Foundation.Interfaces;
 
 namespace EugeneDunin.SchoolSchedule.Foundation.Classes
 {
     public class ClassFactory : IClassFactory<DataModule.Entities.Class>
     {
-        public IClass CreateClass(DataModule.Entities.Class initializer)
+        public IClass CreateClass(SchoolScheduleContext ctx, DataModule.Entities.Class initializer)
         {
-            return new Class() { Number = initializer.Number, Label = initializer.Label };
+            return new Class(ctx, initializer.ClassId) { Number = initializer.Number, Label = initializer.Label };
         }
     }
 }
--- EugeneDunin.DataModule/EugeneDunin.SchoolSchedule.Foundation/TeacherWorkloads/TeacherWorkloadProvider.cs
+++ EugeneDunin.DataModule/EugeneDunin.SchoolSchedule.Foundation/TeacherWorkloads/TeacherWorkloadProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using EugeneDunin.SchoolSchedule.DataModule.Contexts;
 using EugeneDunin.SchoolSchedule.DataModule.Entities;
+using EugeneDunin.SchoolSchedule.Foundation.ClassLoads;
 using EugeneDunin.SchoolSchedule.Foundation.Interfaces;
 
 namespace EugeneDunin.SchoolSchedule.Foundation.TeacherWorkloads
@@ -10,52 +11,112 @@ namespace EugeneDunin.SchoolSchedule.Foundation.TeacherWorkloads
     public class TeacherWorkloadProvider : ITeacherWorkloadProvider
     {
         private readonly SchoolScheduleContext _ctx;
-        private readonly ITeacherWorkloadFactory _teacherWorkloadFactory;
+        private readonly IClassFactory<Class> _classFactory;
 
-
-        public TeacherWorkloadProvider(SchoolScheduleContext ctx, ITeacherWorkloadFactory teacherWorkloadFactory)
+        public TeacherWorkloadProvider(
+            SchoolScheduleContext ctx,
+            IClassFactory<Class> classFactory)
         {
             _ctx = ctx;
-            _teacherWorkloadFactory = teacherWorkloadFactory;
+            _classFactory = classFactory;
         }
 
 
-        public ICollection<ITeacherWorkload> GetTecherWorkLoad(long teacherId, DateTime fromDate, DateTime toDate)
+        public ICollection<ITeacherWorkload> GetTeacherWorkLoad(long teacherId)
         {
-            var teacherWorkloadSchedules = _ctx.TeacherWorkloadSchedules.Where(teacherWorkload =>
-                teacherWorkload.FkTeacherId == teacherId 
-                && teacherWorkload.FromDate >= fromDate
-                && teacherWorkload.ToDate <= toDate)
-                .ToList();
+            return GetTeacherWorkLoad(teacherId, (tws, ts) => tws.FkTeacherSubjectId == ts.TeacherSubjectId);
 
-            return CreateTeacherWorkloads(teacherWorkloadSchedules);
+            /*var teacher = _ctx.Teachers.Find(teacherId);
+
+            return (ICollection<ITeacherWorkload>)
+                    (from ts in _ctx.TeacherSubjects
+                    where ts.FkTeacherId == teacher.TeacherId
+                    join subj in _ctx.Subjects on ts.FkSubjectId equals subj.SubjectId
+                    select new TeacherWorkload
+                    {
+                        TeacherFullName = string.Format(@"{0} {1} {2}", teacher.Surname, teacher.Name, teacher.Patronymic),
+                        SubjectName = subj.SubjectName,
+                        ClassLoads = (from tws in _ctx.TeacherWorkloadSchedules
+                                      where tws.FkTeacherSubjectId == ts.TeacherSubjectId
+                                      join cl in _ctx.Classes on tws.FkClassId equals cl.ClassId
+                                      select CreateClassLoads(cl, tws))
+                                        .ToList()
+                    }).ToList();*/
         }
 
-        public IDictionary<long, ICollection<ITeacherWorkload>> GetTechersWorkLoad(DateTime fromDate, DateTime toDate)
+        public ICollection<ITeacherWorkload> GetTeacherWorkLoad(long teacherId, DateTime fromDate, DateTime toDate)
         {
-            var teacherWorkloadSchedules = _ctx.TeacherWorkloadSchedules
-                .Where(teacherWorkload => teacherWorkload.FromDate >= fromDate && teacherWorkload.ToDate <= toDate)
-                .GroupBy(teacherWorkload => teacherWorkload.TeacherWorkloadScheduleId)
-                .ToList();
+            return GetTeacherWorkLoad(teacherId,
+                ((tws, ts) => tws.FkTeacherSubjectId == ts.TeacherSubjectId && tws.FromDate >= fromDate && tws.ToDate <= toDate));
+        }
 
-            IDictionary<long, ICollection<ITeacherWorkload>> result = new Dictionary<long, ICollection<ITeacherWorkload>>();
-            teacherWorkloadSchedules.ForEach(teacherWorkLoadGroup =>
-                result.Add(teacherWorkLoadGroup.Key, CreateTeacherWorkloads(teacherWorkLoadGroup.AsEnumerable()))
-                );
+        public ICollection<ITeacherWorkload> GetTeachersWorkLoad(DateTime fromDate, DateTime toDate)
+        {
+            return GetTeachersWorkLoad(null, (tws) => tws.FromDate >= fromDate && tws.ToDate <= toDate);
+        }
 
-            return result;
+        public ICollection<ITeacherWorkload> GetTeachersWorkLoad(string subjectName)
+        {
+            return GetTeachersWorkLoad(s => s.SubjectName == subjectName, null);
+        }
+
+        public ICollection<ITeacherWorkload> GetTeachersWorkLoad(string subjectName, DateTime fromDate, DateTime toDate)
+        {
+            return GetTeachersWorkLoad(
+                s => s.SubjectName == subjectName,
+                tws => tws.FromDate >= fromDate && tws.ToDate <= toDate);
         }
 
 
-        private ICollection<ITeacherWorkload> CreateTeacherWorkloads(IEnumerable<TeacherWorkloadSchedule> teacherWorkloads)
+        private ICollection<ITeacherWorkload> GetTeacherWorkLoad(long teacherId, Func<TeacherWorkloadSchedule, TeacherSubject, bool> FilterCondition)
         {
-            var converted = new List<ITeacherWorkload>();
-            foreach (var teacherWorkload in teacherWorkloads)
+            var teacher = _ctx.Teachers.Find(teacherId);
+
+            return (from ts in _ctx.TeacherSubjects
+                where ts.FkTeacherId == teacher.TeacherId
+                join subj in _ctx.Subjects on ts.FkSubjectId equals subj.SubjectId
+                select new TeacherWorkload(_ctx, teacherId)
+                {
+                    Name = teacher.Name,
+                    Surname = teacher.Surname,
+                    Patronymic = teacher.Patronymic,
+                    SubjectName = subj.SubjectName,
+                    ClassLoads = (from tws in _ctx.TeacherWorkloadSchedules
+                            where FilterCondition(tws, ts)
+                            join cl in _ctx.Classes on tws.FkClassId equals cl.ClassId
+                            select CreateClassLoads(cl, tws))
+                        .ToList()
+                }).ToList() as ICollection<ITeacherWorkload>;
+        }
+
+        private ICollection<ITeacherWorkload> GetTeachersWorkLoad(Func<Subject, bool> subjectFilter, Func<TeacherWorkloadSchedule, bool> twsFilter)
+        {
+            return (from t in _ctx.Teachers
+                join ts in _ctx.TeacherSubjects on t.TeacherId equals ts.FkTeacherId
+                join subj in _ctx.Subjects on ts.FkSubjectId equals subj.SubjectId
+                where subjectFilter(subj)
+                select new TeacherWorkload(_ctx, t.TeacherId)
+                {
+                    Name = t.Name,
+                    Surname = t.Surname,
+                    Patronymic = t.Patronymic,
+                    SubjectName = subj.SubjectName,
+                    ClassLoads = (from tws in _ctx.TeacherWorkloadSchedules
+                            where tws.FkTeacherSubjectId == ts.TeacherSubjectId && twsFilter(tws)
+                            join cl in _ctx.Classes on tws.FkClassId equals cl.ClassId
+                            select CreateClassLoads(cl, tws))
+                        .ToList()
+                }).ToList() as ICollection<ITeacherWorkload>;
+        }
+
+        private IClassLoad CreateClassLoads(
+            Class cl, TeacherWorkloadSchedule teacherWorkloadSchedule)
+        {
+            return new ClassLoad(_ctx, teacherWorkloadSchedule.TeacherWorkloadScheduleId)
             {
-                converted.Add(_teacherWorkloadFactory.CreateTeacherWorkload(teacherWorkload));
-            }
-
-            return converted;
+                StudyLoadToClass = teacherWorkloadSchedule.StudyLoad,
+                Class = _classFactory.CreateClass(_ctx, cl)
+            };
         }
     }
 }
