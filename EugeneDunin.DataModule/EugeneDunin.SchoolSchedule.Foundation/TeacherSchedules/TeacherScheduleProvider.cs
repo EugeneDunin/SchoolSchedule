dafaac7f254a842b449d6ceadadbb3ab9d5bb892
Eugene <zhenya.dunin1998@mail.ru>
2019-09-03 13:49:11 +0300

    Finish implementation of TeacherScheduleProvider.

--- EugeneDunin.DataModule/EugeneDunin.SchoolSchedule.Foundation/TeacherSchedules/TeacherScheduleProvider.cs
+++ EugeneDunin.DataModule/EugeneDunin.SchoolSchedule.Foundation/TeacherSchedules/TeacherScheduleProvider.cs
@@ -34,16 +34,17 @@ namespace EugeneDunin.SchoolSchedule.Foundation.TeacherSchedules
 
         public ICollection<ITeacherSchedule> GetTeacherSchedule(long teacherId)
         {
-            var teacher = _ctx.Teachers.Find(teacherId);
+            return GetTeacherSchedule(teacherId, (tws, ts) => tws.FkTeacherSubjectId == ts.TeacherSubjectId);
+
+            /*var teacher = _ctx.Teachers.Find(teacherId);
             _ctx.Entry(_ctx.Teachers.Find(teacherId)).Reference(t => t.Class).Load();
 
-            return (from t in _ctx.Teachers
-                where t.TeacherId == teacherId
-                join ts in _ctx.TeacherSubjects on t.TeacherId equals ts.FkTeacherId
-                join subj in _ctx.Subjects on ts.FkSubjectId equals subj.SubjectId
-                select new TeacherSchedule(_ctx, teacherId)
+            return (from ts in _ctx.TeacherSubjects
+                    where ts.FkTeacherId == teacher.TeacherId
+                    join subj in _ctx.Subjects on ts.FkSubjectId equals subj.SubjectId
+                select new TeacherSchedule(teacherId)
                 {
-                    Teacher = _teacherFactory.CreateTeacher(new TeacherFactoryInitializer(t, subj)),
+                    Teacher = _teacherFactory.CreateTeacher(new TeacherFactoryInitializer(teacher, subj)),
                     OwnClass = _classFactory.CreateClass(teacher.Class),
                     Lessons = (from twsRecord in _ctx.TeacherWorkloadSchedules
                                where twsRecord.FkTeacherSubjectId == ts.TeacherSubjectId
@@ -51,27 +52,72 @@ namespace EugeneDunin.SchoolSchedule.Foundation.TeacherSchedules
                                join clr in _ctx.Classrooms on twsRecord.FkClassroomId equals clr.ClassroomId
                                select _lessonFactory.CreateLesson(new LessonFactoryInitializer(twsRecord, cl, clr)))
                                .ToList()
-                }).ToList() as ICollection<ITeacherSchedule>;
+                }).ToList() as ICollection<ITeacherSchedule>;*/
         }
 
-        public ICollection<ITeacherSchedule> GetTeacherSchedule(long teacherId, DateTime start, DateTime end)
+        public ICollection<ITeacherSchedule> GetTeacherSchedule(long teacherId, DateTime fromDate, DateTime toDate)
         {
-            throw new NotImplementedException();
+            return GetTeacherSchedule(teacherId,
+                ((tws, ts) => tws.FkTeacherSubjectId == ts.TeacherSubjectId && tws.FromDate >= fromDate && tws.ToDate <= toDate));
         }
 
         public ICollection<ITeacherSchedule> GetTeachersSchedule(string subjectName)
         {
-            throw new NotImplementedException();
+            return GetTeachersSchedule(s => s.SubjectName == subjectName, null);
         }
 
-        public ICollection<ITeacherSchedule> GetTeachersSchedule(string subjectName, DateTime start, DateTime end)
+        public ICollection<ITeacherSchedule> GetTeachersSchedule(string subjectName, DateTime fromDate, DateTime toDate)
         {
-            throw new NotImplementedException();
+            return GetTeachersSchedule(
+                s => s.SubjectName == subjectName,
+                tws => tws.FromDate >= fromDate && tws.ToDate <= toDate);
         }
 
-        public ICollection<ITeacherSchedule> GetTeachersWorkLoad(DateTime start, DateTime end)
+        public ICollection<ITeacherSchedule> GetTeachersWorkLoad(DateTime fromDate, DateTime toDate)
         {
-            throw new NotImplementedException();
+            return GetTeachersSchedule(null, (tws) => tws.FromDate >= fromDate && tws.ToDate <= toDate);
+        }
+
+
+        private ICollection<ITeacherSchedule> GetTeacherSchedule(long teacherId, Func<TeacherWorkloadSchedule, TeacherSubject, bool> FilterCondition)
+        {
+            var teacher = _ctx.Teachers.Find(teacherId);
+            _ctx.Entry(_ctx.Teachers.Find(teacherId)).Reference(t => t.Class).Load();
+
+            return (from ts in _ctx.TeacherSubjects
+                where ts.FkTeacherId == teacher.TeacherId
+                join subj in _ctx.Subjects on ts.FkSubjectId equals subj.SubjectId
+                select new TeacherSchedule(teacherId)
+                {
+                    Teacher = _teacherFactory.CreateTeacher(new TeacherFactoryInitializer(teacher, subj)),
+                    OwnClass = _classFactory.CreateClass(teacher.Class),
+                    Lessons = (from twsRecord in _ctx.TeacherWorkloadSchedules
+                            where FilterCondition(twsRecord, ts)
+                            join cl in _ctx.Classes on twsRecord.FkClassId equals cl.ClassId
+                            join clr in _ctx.Classrooms on twsRecord.FkClassroomId equals clr.ClassroomId
+                            select _lessonFactory.CreateLesson(new LessonFactoryInitializer(twsRecord, cl, clr)))
+                            .ToList()
+                }).ToList() as ICollection<ITeacherSchedule>;
+        }
+
+        private ICollection<ITeacherSchedule> GetTeachersSchedule(Func<Subject, bool> subjectFilter, Func<TeacherWorkloadSchedule, bool> twsFilter)
+        {
+
+            return (from t in _ctx.Teachers
+                join ts in _ctx.TeacherSubjects on t.TeacherId equals ts.FkTeacherId
+                join subj in _ctx.Subjects on ts.FkSubjectId equals subj.SubjectId
+                where subjectFilter(subj)
+                select new TeacherSchedule(t.TeacherId)
+                {
+                    Teacher = _teacherFactory.CreateTeacher(new TeacherFactoryInitializer(t, subj)),
+                    OwnClass = _classFactory.CreateClass(t.Class),
+                    Lessons = (from twsRecord in _ctx.TeacherWorkloadSchedules
+                            where twsRecord.FkTeacherSubjectId == ts.TeacherSubjectId && twsFilter(twsRecord)
+                            join cl in _ctx.Classes on twsRecord.FkClassId equals cl.ClassId
+                            join clr in _ctx.Classrooms on twsRecord.FkClassroomId equals clr.ClassroomId
+                            select _lessonFactory.CreateLesson(new LessonFactoryInitializer(twsRecord, cl, clr)))
+                        .ToList()
+                }).ToList() as ICollection<ITeacherSchedule>;
         }
     }
 }
